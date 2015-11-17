afbf7388d31e5d3fcccf4ccf77e0b0feed110915
nguyendk82 <nguyendk82@gmail.com>
2015-11-17 17:41:39 +0000

    initial commit

---
+++ algorithm/stacks/stacks.js
@@ -0,0 +1,21 @@
+function stack(){
+    //constructor, stack take no argument
+    //storage object
+    //initial size
+    
+    // methods
+    
+    // push take one argumaent, value
+    // use size as key, value
+    // increment the size
+    // return new size
+    
+    // pop take no argument
+    // decrease the size
+    // use size-1 as key
+    // delete key, value
+    // return the value
+    
+    // size take no argument
+    // return the size
+}
